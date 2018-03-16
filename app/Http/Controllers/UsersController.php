79bd64738a28836c31641c68d1bc9e5129c937e2
zouyou <1075820342@qq.com>
2018-03-16 17:20:31 +0800

    权限控制，仅允许用户修改自己的信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,16 +9,24 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        // 除了show个人信息的展示都要经过auth中间件
+        $this->middleware('auth', ['except' => ['show']]);
+    }
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
     public function edit(User $user)
     {
+        // $user为根据get传的id自动查出来的user的集合，用authorize方法调用授权策略，自动与当前登录的用户进行比较
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         // 如果上传了图片则保存信息
