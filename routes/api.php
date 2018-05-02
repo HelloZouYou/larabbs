434028644377e55d593e7a91f22b07fe6f019b7c
zouyou-home-linux <hellozouyou@gmail.com>
2018-05-02 15:20:29 +0000

    add captcha

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,10 @@ $api->version('v1', [
 		$api->post('users', 'UsersController@store')
 		    ->name('api.users.store');
 	});
+	// 用户注册
+	$api->post('users', 'UsersController@store')
+	    ->name('api.users.store');
+	// 图片验证码
+	$api->post('captchas', 'CaptchasController@store')
+	    ->name('api.captchas.store');
 });
